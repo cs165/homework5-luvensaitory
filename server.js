4de8a2b3d44759ad53218eb1185428255bd6a4f7
luvensaitory <luvensaitory@gmail.com>
2019-06-03 22:43:09 +0800

    done.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const key = require('./privateSettings.json');
 
 // TODO(you): Change the value of this string to the spreadsheet id for your
 // GSA spreadsheet. See HW5 spec for more information.
-const SPREADSHEET_ID = '__YOUR__SPREADSHEET__ID__HERE__';
+const SPREADSHEET_ID = '1hrS5PEDhghrpw94Uh6hRxAgXdUftnLNfHqd48fB3EX0';
 
 const app = express();
 const jsonParser = bodyParser.json();
@@ -15,50 +15,71 @@ const sheet = googleSheets(key.client_email, key.private_key, SPREADSHEET_ID);
 app.use(express.static('public'));
 
 async function onGet(req, res) {
-  const result = await sheet.getRows();
-  const rows = result.rows;
-  console.log(rows);
-
-  // TODO(you): Finish onGet.
-
-  res.json( { status: 'unimplemented'} );
+    const result = await sheet.getRows();
+    const rows = result.rows;
+    const ans = [];
+    for (let i = 1; i < rows.length; i++) {
+        temp = {}
+        temp[rows[0][0]] = rows[i][0];
+        temp[rows[0][1]] = rows[i][1];
+        ans.push(temp);
+    }
+    res.json(ans);
 }
 app.get('/api', onGet);
 
 async function onPost(req, res) {
-  const messageBody = req.body;
-
-  // TODO(you): Implement onPost.
-
-  res.json( { status: 'unimplemented'} );
+    const messageBody = req.body;
+    const result = await sheet.getRows();
+    const rows = result.rows;
+    let idx = rows.length;
+    const ans = [];
+    for (let i = 0; i < rows[0].length; i++) {
+        ans.push(messageBody[rows[0][i]]);
+    }
+    const status = await sheet.setRow(idx, ans);
+    res.json(status);
 }
 app.post('/api', jsonParser, onPost);
 
 async function onPatch(req, res) {
-  const column  = req.params.column;
-  const value  = req.params.value;
-  const messageBody = req.body;
+    const column = req.params.column;
+    const value = req.params.value;
+    const messageBody = req.body;
 
-  // TODO(you): Implement onPatch.
+    // TODO(you): Implement onPatch.
 
-  res.json( { status: 'unimplemented'} );
+    res.json({ status: 'unimplemented' });
 }
 app.patch('/api/:column/:value', jsonParser, onPatch);
 
 async function onDelete(req, res) {
-  const column  = req.params.column;
-  const value  = req.params.value;
-
-  // TODO(you): Implement onDelete.
-
-  res.json( { status: 'unimplemented'} );
+    const column = req.params.column; //name
+    const value = req.params.value; //Bort
+    const result = await sheet.getRows();
+    const rows = result.rows;
+    let idx = -1;
+    for (let i = 0; i < rows[0].length; i++) {
+        if (rows[0][i] === column) {
+            idx = i;
+            break;
+        }
+    }
+    let ans;
+    for (let i = 1; i < rows.length; i++) {
+        if (rows[i][idx] === value) {
+            ans = await sheet.deleteRow(i);
+            break;
+        }
+    }
+    res.json(ans);
 }
-app.delete('/api/:column/:value',  onDelete);
+app.delete('/api/:column/:value', onDelete);
 
 
 // Please don't change this; this is needed to deploy on Heroku.
 const port = process.env.PORT || 3000;
 
-app.listen(port, function () {
-  console.log(`Server listening on port ${port}!`);
+app.listen(port, function() {
+    console.log(`Server listening on port ${port}!`);
 });
